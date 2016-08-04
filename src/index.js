ad4ea86c3cd43f32f41b2e291d8c540f807c67f9
Jordan Klassen <jordan@klassen.me.uk>
2016-08-04 09:52:48 -0700

    feat: support entries, keys, values

--- src/index.js
+++ src/index.js
@@ -1,10 +1,40 @@
 
-export default function reversed(arr) {
-  return {
-    *[Symbol.iterator]() {
-      for (let i = arr.length - 1; i >= 0; i--) {
-        yield arr[i];
-      }
+class ArrayLikeReversed {
+  constructor(value) {
+    this._value = value;
+  }
+
+  * [Symbol.iterator]() {
+    for (let i = this._value.length - 1; i >= 0; i--) {
+      yield this._value[i];
     }
-  };
+  }
+
+  * entries() {
+    for (let i = this._value.length - 1; i >= 0; i--) {
+      yield [i, this._value[i]];
+    }
+  }
+
+  * keys() {
+    for (let i = this._value.length - 1; i >= 0; i--) {
+      yield i;
+    }
+  }
+
+  * values() {
+    for (let i = this._value.length - 1; i >= 0; i--) {
+      yield this._value[i];
+    }
+  }
 }
+
+export default function reversed(value) {
+  if (value.length) {
+    return new ArrayLikeReversed(value);
+  }
+  throw new Error('Cannot reverse given object without length');
+}
+
+export {ArrayLikeReversed as ArrayLike};
+export {ArrayLikeReversed as Array};
--- test/index.test.js
+++ test/index.test.js
@@ -10,3 +10,33 @@ test('iterator', (t) => {
   }
   t.pass()
 })
+
+test('entries', (t) => {
+  let expected = 8
+  let expectedIndex = 2
+  for (const [i, n] of reversed([6, 7, 8]).entries()) {
+    t.true(n === expected)
+    expected--
+    t.true(i === expectedIndex)
+    expectedIndex--
+  }
+  t.pass()
+})
+
+test('keys', (t) => {
+  let expectedIndex = 2
+  for (const i of reversed([6, 7, 8]).keys()) {
+    t.true(i === expectedIndex)
+    expectedIndex--
+  }
+  t.pass()
+})
+
+test('values', (t) => {
+  let expected = 8
+  for (const n of reversed([6, 7, 8]).values()) {
+    t.true(n === expected)
+    expected--
+  }
+  t.pass()
+})
